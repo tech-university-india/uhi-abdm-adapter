b2ee2b90a9a7e2804c866cee739beb452126987e
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-20 20:12:08 +0530

    feat: Create Transition

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const { client } = require('./src/util/redisConnection')
+const { getJWTToken } = require('./src/util/middleware/authToken')
+const { routeController } = require('./src/controllers/abdmController')
 
+client.connect()
+client.on('connect', () => console.log('Redis client connected'))
 app.use(express.json())
 app.use(express.text())
-
+app.use(getJWTToken)
 const PORT = process.env.PORT || 9007
 
-app.use('*', (req, res) => {
-  res.sendStatus(200)
-  res.json({ status: 'OK' })
-})
+app.use('*', routeController)
 
 app.listen(PORT, () => console.log(`Started on port ${PORT}`))
