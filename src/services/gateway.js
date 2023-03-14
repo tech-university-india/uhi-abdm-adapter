2a2dd51fbb7dd0ab40e0bb94be8b48710cffba74
SaatwikRishi <saatwik_s@mckinsey.com>
2023-03-14 18:53:43 +0530

    add sandbox url option for gateway

--- src/services/gateway.js
+++ src/services/gateway.js
@@ -1,15 +1,29 @@
 const cacheAxios = require('../utils/axios')
 
 // Change this to custom aws lambda route while running locally - krke
+const SANDBOX_URL = process.env.SANDBOX_URL
 const getToken = async () => {
-  const { accessToken } = await cacheAxios({
-    method: 'POST',
-    url: `${process.env.GATEWAY_URL}/v0.5/sessions`,
-    data: {
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    }
-  }, true)
+  if (SANDBOX_URL) {
+    const { accessToken } = await cacheAxios(
+      {
+        url: SANDBOX_URL
+      },
+      true
+    )
+    return accessToken
+  }
+
+  const { accessToken } = await cacheAxios(
+    {
+      method: 'POST',
+      url: `${process.env.GATEWAY_URL}/v0.5/sessions`,
+      data: {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+      }
+    },
+    true
+  )
   return accessToken
 }
 
