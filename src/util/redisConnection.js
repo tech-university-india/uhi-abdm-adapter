6570579f8e94578fba875c1866854d19f6245897
SaatwikRishi <saatwik_s@mckinsey.com>
2023-03-14 12:46:09 +0530

    chore: modify env example and redis connection file

--- src/util/redisConnection.js
+++ src/util/redisConnection.js
@@ -1,13 +1,21 @@
 const redis = require('redis')
 
+const USE_URL = process.env.USE_REDIS_URL || false
+const REDIS_URL = process.env.REDIS_URL
+
 const options = {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  },
+
+  username: process.env.REDIS_USERNAME,
   name: process.env.REDIS_NAME,
   database: process.env.REDIS_DB,
   password: process.env.REDIS_PASSWORD
 }
 
-const client = redis.createClient(options)
+const finalOptions = USE_URL ? { url: REDIS_URL } : options
+const client = redis.createClient(finalOptions)
 
 module.exports = { client }
